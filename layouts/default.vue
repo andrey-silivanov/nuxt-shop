bc89a27741713103e148636be8735658780592c4
a_silivanov <andrey.silivanovv@gmail.com>
2018-10-10 18:16:54 +0300

    animate

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,14 +3,17 @@
     <!-- Header -->
     <app-header/>
     <nuxt/>
+    <app-footer/>
   </div>
 </template>
 <script>
   import AppHeader from '~/components/AppHeader.vue';
+  import AppFooter from '~/components/AppFooter';
 
   export default {
     components: {
-      AppHeader
+      AppHeader,
+      AppFooter
     }
   }
 </script>
